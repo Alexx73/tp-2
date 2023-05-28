21745e6ec8b8d781bfac175c518de4097b8146fd
alexx73 <tristanluna@yahoo.com.ar>
2023-05-28 19:31:12 -0300

    calculos por edad funcionan

--- Program.cs
+++ Program.cs
@@ -111,19 +111,26 @@ namespace cajero_app2
 
         // ---------------------------------------------------------
 
-        static public (int, double) CargarGrupos() 
+        static public (int,int, double) CargarGrupos() 
         {
-            int edad, edad_grupo;
-            double total_grupo, entradas, descuento;
+            int edad, edad_grupo, mayor, menor1, menor2, menor3, entradas_con_descuento;
+            double total_grupo, total_sin_desc, entradas, descuento;
+            bool aceptado = false;
             descuento = 0;
             total_grupo = 0;
             entradas = 0;
             edad = 0;
             edad_grupo = 0;
+            menor1 = 0;
+            menor2 = 0;
+            menor3 = 0;
+            mayor = 0;
+            entradas_con_descuento = 0;
+            total_sin_desc = 0;
             
             do 
             {
-                            Console.Clear();
+            Console.Clear();
             Console.WriteLine("=== Cargar grupo ===");
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("ingresar -1 para salir");
@@ -134,32 +141,79 @@ namespace cajero_app2
             edad = Convert.ToInt32(Console.ReadLine()); 
             if (edad != -1) 
             {
-                edad_grupo += edad;
-                entradas ++;
+                // edad_grupo += edad;
+                // entradas ++;
+            
+
+            if ( edad < 4 ) // edad > 0 &
+                {
+                    menor1 = menor1 +1 ;
+                    
+                }
+            if (edad >3 && edad < 11)
+                {
+                    menor2 = menor2 +1 ;
+                }
+            if (edad >10 && edad < 16)
+                {
+                    menor3= menor3 + 1;
+                } 
+                if ( edad > 15)
+                {
+                   mayor = mayor +1 ;
+                } 
+               
             }
             }
             while(edad != -1);
+            entradas_con_descuento = menor2 + menor3 + mayor ; // numero total de entradas con descuento
+            entradas = menor1 + entradas_con_descuento ;
+            total_sin_desc = (mayor * 1500)  + (menor2 * 750) + (menor3 * 1200)   ;
 
-            if ( entradas >= 4) 
-                 
-            {
-                descuento = (entradas * 1500) /100 * 15;
-                total_grupo = (entradas * 1500) - descuento;
+            if ( entradas_con_descuento > 3)            
+            {       
+                descuento = total_sin_desc /100 * 15;
+                // total_grupo = total_sin_desc - descuento + ( menor1 * 150) ;
+
             } else  {
-                total_grupo = (entradas * 1500);
+                // total_grupo =  ( menor1 * 150) +  total_grupo;
             }
+            total_grupo = total_sin_desc - descuento + ( menor1 * 150) ;
 
-            
+
+            // Console.WriteLine("el total en pesos es: {0}", total_grupo );
+            // Console.WriteLine("Aceptar o Cancelar");
+            // string opcion = Console.ReadLine();
+            // switch (opcion)
+            // {
+            //     case "a":
+            //         Console.WriteLine("Has seleccionado Aceptar.");
+            //         aceptado = true; // acumular valores
+            //         break;
+            //     case "c":
+            //         Console.WriteLine("Has seleccionado Cancelar.");
+                    
+            //         break;
+            //     default:
+            //         Console.WriteLine("Opción inválida. Por favor, seleccione una opción válida.");
+            //         break;
+            // }
+            // Console.WriteLine();
+
+            // Console.WriteLine("la edad total es: {0}", edad_grupo );
+            Console.WriteLine("la cantidad de entradas menores es: {0}",menor1 );
+                        Console.WriteLine("la cantidad de entradas menor2 es: {0}",menor2 );
+            Console.WriteLine("la cantidad de entradas menor3 es: {0}",menor3 );
+            Console.WriteLine("la cantidad de entradas mayor es: {0}",mayor );
 
 
-            Console.WriteLine("la edad total es: {0}", edad_grupo );
-            Console.WriteLine("la cantida de entradas es: {0}", entradas );
+            Console.WriteLine("la cantidad de entradas es: {0}",entradas );
             Console.WriteLine("el total en pesos es: {0}", total_grupo );
 
             Console.WriteLine("presione una tecla para volver al menu principal");
-            Console.ReadKey();                                 
-            Console.WriteLine("la edad total es: {0}", edad_grupo );
-            return (edad_grupo, total_grupo) ;
+            Console.ReadKey();        
+            // Console.WriteLine("la edad total es: {0}", edad_grupo );
+            return ( edad_grupo, edad, total_grupo) ;
         }
     }
 }
