dd15b478ff97f7a8efd55cf0c00b7e26e947dfeb
alexx73 <tristanluna@yahoo.com.ar>
2023-05-30 00:36:48 -0300

    ver datos, pasaporte

--- Program.cs
+++ Program.cs
@@ -13,9 +13,18 @@ namespace cajero_app2
 
         static void Main(string[] args)
         {
+            // Ventana 
+ // Obtiene el ancho de la consola
+        int consoleWidth = Console.WindowWidth;
+
+        // Calcula la posición horizontal para centrar el menú
+        int menuWidth = 20; // Anchura del menú
+        int leftMargin = (consoleWidth - menuWidth) / 2;
+
+
             // Salidas  
-            int total_de_grupos, personas_total;
-            double rec_pasaportes, individuales, recaudacion_total, edad_promedio; 
+            int total_de_grupos, personas_total, personas_parcial;
+            double rec_pasaportes, individuales, recaudacion_total, edad_promedio , pasaportes_parcial, pasarportes_total; 
 
             int personas_con_pasaporte, personas_diez_quince;
             int operacion, edad_total , edad, total_grupos_parcial;
@@ -26,6 +35,10 @@ namespace cajero_app2
             recaudacion_acumulada = 0;
             recaudacion= 0;
             total_grupos_parcial = 0;
+            personas_total =0;
+            personas_parcial = 0;
+            pasaportes_parcial =0;
+            pasarportes_total = 0;
             // saldo = 100;
             // int operacion;
 
@@ -45,9 +58,14 @@ namespace cajero_app2
                         Console.Clear();
                         // return ( edad_grupo, edad, total_de_grupos, total_grupo) ;
                         // CargarGrupos() ;
-                        (edad_total, edad, total_grupos_parcial, recaudacion) = CargarGrupos();
-                        recaudacion_acumulada = recaudacion_acumulada + recaudacion;
+                        (total_grupos_parcial, personas_parcial , pasaportes_parcial, edad, recaudacion) = CargarGrupos();
                         total_de_grupos = total_de_grupos + total_grupos_parcial ;
+                        personas_total = personas_total + personas_parcial;
+                        pasarportes_total = pasarportes_total + pasaportes_parcial;
+
+                        recaudacion_acumulada = recaudacion_acumulada + recaudacion;
+
+                         Console. ResetColor();
                         // edad_grupo, edad, total_grupo
 
                         // Console.WriteLine("Cargar grupo");
@@ -59,7 +77,7 @@ namespace cajero_app2
                 case 2:
                     {
                         Console.Clear();
-                        VerDatos(total_de_grupos, recaudacion_acumulada, recaudacion );
+                        VerDatos(total_de_grupos,personas_total, recaudacion_acumulada,pasarportes_total, recaudacion );
                         // Console.WriteLine("Ver reporte");
                         Console.Beep(440, 300);
                         Console.ReadKey();                     
@@ -87,7 +105,8 @@ namespace cajero_app2
 
         //--------------------------------------------------
 
-         static void VerDatos(int total_de_Grupos, double recaudacion_total, double recaudacion_parcial)
+         static void VerDatos(int total_de_Grupos,int personas_total, double pasaportes,
+          double recaudacion_total, double recaudacion_parcial)
         {
  
                // Obtiene el ancho de la consola
@@ -100,11 +119,14 @@ namespace cajero_app2
                     Console.Clear();
 
             // Console.WriteLine("Ver reporte");
-
+        Console.ForegroundColor = ConsoleColor.Yellow;
         Console.WriteLine("".PadLeft(leftMargin) + "===== DATOS DEL DIA =====");
         Console.WriteLine("".PadLeft(leftMargin) + "TOTAL GRUPOS: {0}", total_de_Grupos);
-        Console.WriteLine("".PadLeft(leftMargin) + "TOTAL PERSONAS:");
-        Console.WriteLine("".PadLeft(leftMargin) + "RECAUDACION: {0} ", recaudacion_total);
+        Console.WriteLine("".PadLeft( leftMargin) + "TOTAL PERSONAS: {0}", personas_total );
+        Console.WriteLine("".PadLeft(leftMargin) + "RECAUDACION PASAPORTES: {0} ", pasaportes);
+
+
+        Console.WriteLine("".PadLeft(leftMargin) + "RECAUDACION TOTAL: {0} ", recaudacion_total);
         Console.WriteLine("".PadLeft(leftMargin) +"==========================");
         Console.ReadKey();        
         // return;
@@ -153,12 +175,13 @@ namespace cajero_app2
 
         // ---------------------------------------------------------
 
-        static public (int, int, int, double) CargarGrupos() 
+        static public (int, int, double,int, double) CargarGrupos() 
         //edad_grupo, edad, total_grupo
         {
-            int edad, edad_grupo, mayor, menor1, menor2, menor3, entradas_con_descuento, total_de_grupos;
-            double total_grupo, total_sin_desc, entradas, descuento;
+            int edad, edad_grupo, mayor, menor1, menor2, menor3, entradas_con_descuento, total_de_grupos, personas;
+            double total_grupo, total_sin_desc, entradas, descuento, entradas_pasaporte;
             bool aceptado = false;
+            string edad_string ="", edades_grupo ="";
             total_de_grupos =0;
             descuento = 0;
             total_grupo = 0;
@@ -171,18 +194,64 @@ namespace cajero_app2
             mayor = 0;
             entradas_con_descuento = 0;
             total_sin_desc = 0;
+            personas = 0;
+            entradas_pasaporte =0;
+            int fila =0;
+
+            /////////////////////////////////////////////////
+
+            // FONDO
+            // Console.BackgroundColor = ConsoleColor.Blue;
+
+            // Obtiene el ancho de la consola
+             int paddingTop = 5;
+        int consoleWidth = Console.WindowWidth;
+        // Console.WindowWidth = 40;
+        // int consoleWidth =Console.WindowWidth;
+
+        // Calcula la posición horizontal para centrar el menú
+        int menuWidth = 20; // Anchura del menú
+        int leftMargin = (consoleWidth - menuWidth) / 2;
+
+        Console.Title = ("Paseo Aventura");
+        // Console.BackgroundColor = ConsoleColor.Gray;
+                
+        // Establece la posición del cursor para centrar el menú
+        Console.SetCursorPosition(leftMargin, Console.CursorTop);
+        Console.Clear();
+            for (int i = 0; i < paddingTop; i++)
+        {
+            Console.WriteLine();
+        }
             
             do 
             {
             Console.Clear();
-            Console.WriteLine("=== Cargar grupo ===");
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("ingresar -1 para salir");
-
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("".PadLeft(leftMargin) + "==== Cargar grupo ====");
             Console.ResetColor();
-            Console.WriteLine("ingresar edad");
 
-            edad = Convert.ToInt32(Console.ReadLine()); 
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("".PadLeft(leftMargin) + "ingresar -1 para salir");
+            Console.ResetColor();
+           
+                        Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("".PadLeft(leftMargin) + "ingresar edad");
+            Console.SetCursorPosition(95, 0 );
+            Console.WriteLine("".PadLeft(leftMargin) + edades_grupo);
+           Console.SetCursorPosition(80, 3);
+           edad_string = Console.ReadLine();
+
+            // Console.SetCursorPosition(60, 2);
+            // Console.WriteLine(edades_grupo);
+            edades_grupo = edades_grupo +  "  \n" + edad_string;
+            // Console.SetCursorPosition(75, 9);
+            // Console.WriteLine(edades_grupo);
+            fila = fila +1;
+            Thread.Sleep(1000);
+            edad = Convert.ToInt32(edad_string); 
             if (edad != -1) 
             {
                 // edad_grupo += edad;
@@ -217,11 +286,15 @@ namespace cajero_app2
             if ( entradas_con_descuento > 3)            
             {       
                 descuento = total_sin_desc /100 * 15;
+                entradas_pasaporte = total_sin_desc - descuento;
+               
                 // total_grupo = total_sin_desc - descuento + ( menor1 * 150) ;
 
             } else  {
                 // total_grupo =  ( menor1 * 150) +  total_grupo;
             }
+            personas = entradas_con_descuento + menor1;
+            // entradas_pasaporte = total_sin_desc - descuento;
             total_grupo = total_sin_desc - descuento + ( menor1 * 150) ;
             total_de_grupos = total_de_grupos +1;
 
@@ -258,7 +331,8 @@ namespace cajero_app2
             Console.WriteLine("presione una tecla para volver al menu principal");
             Console.ReadKey();        
             // Console.WriteLine("la edad total es: {0}", edad_grupo );
-            return ( edad_grupo, edad, total_de_grupos, total_grupo) ;
+           
+            return ( total_de_grupos, personas, entradas_pasaporte, edad, total_grupo) ;
         }
     }
 }
